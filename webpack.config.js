e90635ae3ec8fc05ada34bb934446eb612b091cc
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-10-31 15:53:30 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-undef */
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// eslint-disable-next-line no-undef
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
